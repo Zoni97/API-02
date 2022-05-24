ffe9ccbfcacedcfb1c41e6ea9551d0c3068a7324
Sonia Curridori <soniacurridori@gmail.com>
2022-05-24 11:33:56 +0200

    file initialization

---
+++ src/test/java/co/develhope/api02/Api02ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.api02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
