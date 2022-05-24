8a8515665535f584e8cb91368e1cb81e4c76ef98
Sonia Curridori <soniacurridori@gmail.com>
2022-05-24 16:49:11 +0200

    exercise

--- src/main/java/co/develhope/api02/controllers/FactorialController.java
+++ src/main/java/co/develhope/api02/controllers/FactorialController.java
@@ -1,2 +1,18 @@
-package co.develhope.api02.controllers;public class FactorialController {
+package co.develhope.api02.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FactorialController {
+
+    @GetMapping("/{id}")
+    public int factorial(@PathVariable int id){
+        int factorial = 1;
+        for(int i=1; i<id; i++) {
+            factorial += factorial*i;
+        }
+        return factorial;
+    }
 }
